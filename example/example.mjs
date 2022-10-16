e02ac08e014880064fd290db349f3499b3d5e6fe
Wany <sung@wany.io>
2022-10-16 22:33:03 +0900

    add files

---
+++ example/example.mjs
@@ -0,0 +1,17 @@
+import Crypto from '../crypto.mjs';
+
+console.log('hash: ' + new Crypto('hello').hash());
+console.log('hash: ' + new Crypto('hello').salt('salt').hash());
+
+const d = 'hello world';
+const k = 'booooooo';
+console.log('original: ' + d);
+const c = new Crypto(d).key(k).cipher();
+console.log('cipher: ' + c);
+const d2 = new Crypto(c).key(k).decipher();
+console.log('decipher: ' + d2);
+
+console.log('random number: ' + Crypto.randomNumber(100));
+
+console.log('random string: ' + Crypto.randomString(16));
+console.log('uid (not UUID): ' + Crypto.uid());
